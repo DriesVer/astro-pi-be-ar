3273d6f1b7e0455f69257ccf2d7b92a87618dafb
DriesVer <34188118+DriesVer@users.noreply.github.com>
2018-02-04 15:23:33 +0000

    Update VegCam.py

--- VegCam.py
+++ VegCam.py
@@ -122,12 +122,14 @@ while(True):
 	
 	imgNumb = imgNumb + 1						#Chaching the image number with 1
 	
-	for i in range(0,5000):
+	"""
+	for i in range(0,40000):
 		print "waiting"
 	sense.set_pixel(7,7,red)					#Setting a pixel int he corner red for indicating that he capture it. Also for showing that he isn't blocked.
-	for i in range(0,5000):
+	for i in range(0,40000):
 		print "waiting"
 	sense.set_pixel(7,7,black)					#Setting a pixel int he corner black for indicating that he capture it. Also for showing that he isn't blocked.
+	"""					#
 	
 	
 #camera.stop_preview()							#Was stopping the camera.
